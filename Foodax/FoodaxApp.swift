348b37d3d53631959eddd03b0c5a6805e67b263f
Kyrylo Panin <kyrylo.panin@globallogic.com>
2023-06-14 17:48:53 +0300

    Initial Commit

---
+++ Foodax/FoodaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodaxApp.swift
+//  Foodax
+//
+//  Created by Kyrylo Panin on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
